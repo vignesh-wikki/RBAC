f1ebfc179726074315cf9ad43ed6cd98ad7756f8
vignesh-wikki <vignesha445@gmail.com>
2024-11-27 22:04:39 +0530

    |

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const connectFlash = require("connect-flash");
+const path = require("path");
+
 const ejs = require("ejs");
 require("dotenv").config();
 
 app.use(cookieParser());
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
